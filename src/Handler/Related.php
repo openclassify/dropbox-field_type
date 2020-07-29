5fed4dc79ed371c1a2852fd59940fc904a24d000
Diatrex <diahshalabi@hotmail.com>
2020-07-29 12:36:40 +0300

    #1840 Paketlere komisyon oranı eklenmesi

--- src/Handler/Related.php
+++ src/Handler/Related.php
@@ -63,7 +63,7 @@ class Related
                             'name' => $category,
                             'ad_limit' => setting_value('visiosoft.module.packages::default_adv_limit'),
                             'time_limit' => setting_value('visiosoft.module.advs::default_published_time'),
-                            'commission' => setting_value('visiosoft.module.packages::default_commission'),
+                            'commission' => setting_value('visiosoft.module.orders::commission'),
                         ];
                     }
                 }
