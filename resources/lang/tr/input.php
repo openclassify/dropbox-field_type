25affbc394068f84f93ec5457d5b28295abe1f62
Metehan Celik <mthncelik@gmail.com>
2022-04-07 12:22:05 +0300

    admin panel translations

--- resources/lang/tr/input.php
+++ resources/lang/tr/input.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-    'placeholder' => 'Bir giriş seçin ...'
+    'placeholder' => 'Bir giriş seçin ...',
+    'create_ad_limit' => 'Reklam sınırı oluştur',
+    'ad_publish_time' => 'Reklam Yayınlama Zamanı Günü',
+    'limit' => 'Limit',
+    'commission' => 'Komisyon',
 ];
